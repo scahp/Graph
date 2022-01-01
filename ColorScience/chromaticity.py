2e0c1e3381a1ffa33d23552eb0d3b4c15d2d6a84
scahp <scahp@naver.com>
2022-01-01 22:03:08 +0900

    Modified variable & function names
    from xyY to XYZ color space.
    because wrong explain about this. the source of XYZ is xyY. but the matrix is only for XYZ <-> sRGB space.

--- ColorScience/chromaticity.py
+++ ColorScience/chromaticity.py
@@ -13,37 +13,36 @@ xw = 0.3127
 yw = 0.3290
 Yw = 1
 
-# white point of xyY color space
-WhitePoint_xyY = [xw, Yw, Yw]
+# white point of XYZ color space
+WhitePoint_XYZ = [xw / yw, Yw, (1.0 - xw - yw) / yw]
 
-def GetLuminance_From_sRGB_Primaries_with_white_of_xyY():
+def GetLuminance_From_sRGB_Primaries_with_white_of_XYZ():
     a = np.array([
           [sRGB_Primary_R[0]/sRGB_Primary_R[1], sRGB_Primary_G[0]/sRGB_Primary_G[1], sRGB_Primary_B[0]/sRGB_Primary_B[1]],
           [1, 1, 1],
           [(1.0-sRGB_Primary_R[0]-sRGB_Primary_R[1])/sRGB_Primary_R[1], (1.0-sRGB_Primary_G[0]-sRGB_Primary_G[1])/sRGB_Primary_G[1], (1.0 - sRGB_Primary_B[0] - sRGB_Primary_B[1])/sRGB_Primary_B[1]]])
     inva = np.linalg.inv(a);
-    whtePoint_xyY = [xw / yw, 1, (1.0 - xw - yw) / yw]
-    Y = np.matmul(inva, whtePoint_xyY)
+    Y = np.matmul(inva, WhitePoint_XYZ)
     return Y
 
-def GetMatrix_sRGB_to_xyY():
-    Y = GetLuminance_From_sRGB_Primaries_with_white_of_xyY()
-    sRGB_to_xyY = np.array([
+def GetMatrix_sRGB_to_XYZ():
+    Y = GetLuminance_From_sRGB_Primaries_with_white_of_XYZ()
+    sRGB_to_XYZ = np.array([
                     [sRGB_Primary_R[0]/sRGB_Primary_R[1]*Y[0], sRGB_Primary_G[0]/sRGB_Primary_G[1]*Y[1], sRGB_Primary_B[0]/sRGB_Primary_B[1]*Y[2]],
                     [Y[0], Y[1], Y[2]],
                     [(1.0-sRGB_Primary_R[0]-sRGB_Primary_R[1])/sRGB_Primary_R[1]*Y[0], (1.0-sRGB_Primary_G[0]-sRGB_Primary_G[1])/sRGB_Primary_G[1]*Y[1], (1.0-sRGB_Primary_B[0]-sRGB_Primary_B[1])/sRGB_Primary_B[1]*Y[2]],
                 ])
-    return sRGB_to_xyY
+    return sRGB_to_XYZ
 
-def GetMatrix_xyY_to_sRGB():
-    sRGB_to_xyY = GetMatrix_sRGB_to_xyY()
-    xyY_to_sRGB = np.linalg.inv(sRGB_to_xyY)
-    return xyY_to_sRGB
+def GetMatrix_XYZ_to_sRGB():
+    sRGB_to_XYZ = GetMatrix_sRGB_to_XYZ()
+    XYZ_to_sRGB = np.linalg.inv(sRGB_to_XYZ)
+    return XYZ_to_sRGB
 
-print('[Yr, Yg, Yb] : {0}'.format(GetLuminance_From_sRGB_Primaries_with_white_of_xyY()))
-print('[sRGB to xyY] : \n{0}'.format(GetMatrix_sRGB_to_xyY()))
-print('[xyY to sRGB] : \n{0}'.format(GetMatrix_xyY_to_sRGB()))
+print('[Yr, Yg, Yb] : {0}'.format(GetLuminance_From_sRGB_Primaries_with_white_of_XYZ()))
+print('[sRGB to XYZ] : \n{0}'.format(GetMatrix_sRGB_to_XYZ()))
+print('[XYZ to sRGB] : \n{0}'.format(GetMatrix_XYZ_to_sRGB()))
 
-whitepoint_sRGB = np.matmul(GetMatrix_xyY_to_sRGB(), WhitePoint_xyY);
+whitepoint_sRGB = np.matmul(GetMatrix_XYZ_to_sRGB(), WhitePoint_XYZ);
 print('[White point of sRGB] : {0}'.format(whitepoint_sRGB))
 
